f2542e58658c62263b208c581683aee49bf752b4
jared <jsgordon212@gmail.com>
2016-07-10 17:02:28 -0400

    fixed login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      redirect_to '/login'
+      redirect_to '/login', notice: "Invalid Username or Password"
     end
   end
 
