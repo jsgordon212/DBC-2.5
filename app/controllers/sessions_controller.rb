c3cc75152e0d109d852e50bd6aa768b0d6ddcbb4
jared <jsgordon212@gmail.com>
2016-07-10 17:15:27 -0400

    changed error form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      @errors = ["Display Name or Password invalid"]
+      @errors = "Display Name or Password invalid"
       render 'new'
     end
   end
